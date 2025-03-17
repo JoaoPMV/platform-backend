d8a70aea477ecc87b59b8a91574033df0592caff
JoaoPMV <joaop.trilha@gmail.com>
2025-03-17 18:41:33 -0300

    commit

--- app.js
+++ app.js
@@ -8,34 +8,31 @@ const PORTA = process.env.PORT || 3000;
 
 const app = express();
 
-app.use("/api/students", studentRoutes);
-// Iniciar o servidor após a conexão com o banco de dados
-app.listen(PORTA, () => {
-  console.log(`Servidor rodando na porta ${PORTA}`);
-});
-
 // Configuração do CORS
 const corsOptions = {
-  origin: "https://platforma-frontend.vercel.app", // Frontend em produção
-  credentials: true, // Permite enviar cookies e tokens entre frontend e backend
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Métodos permitidos
+  origin: "https://platforma-frontend.vercel.app",
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders:
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization", // Cabeçalhos permitidos
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
 };
 
-// Aplicar o middleware CORS globalmente
+// Middleware CORS e JSON antes das rotas
 app.use(cors(corsOptions));
-
-// Middleware para responder às requisições OPTIONS
 app.options("*", cors(corsOptions));
-
 app.use(express.json());
 
 // Conectar ao banco antes de iniciar o servidor
 conn()
-  .then(() => {})
+  .then(() => {
+    app.use("/api/students", studentRoutes);
+
+    app.listen(PORTA, () => {
+      console.log(`Servidor rodando na porta ${PORTA}`);
+    });
+  })
   .catch((err) => {
-    console.error("Não foi possível iniciar o servidor:", err);
+    console.error("Não foi possível conectar ao banco de dados:", err);
   });
 
 // Expor o app para o Vercel gerenciar
